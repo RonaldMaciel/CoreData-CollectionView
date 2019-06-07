1bf3fabe21abf920c13903f8f20452f4ed68bddf
Ronald Maciel <ronaldsmaciel2@outlook.com>
2019-06-07 14:04:24 -0300

    Testing

---
+++ CoreData-CollectionView/CoreData-CollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreData-CollectionView
+//
+//  Created by Ronald Maciel on 07/06/19.
+//  Copyright © 2019 Ronald Maciel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
